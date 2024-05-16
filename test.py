bca2e870423c8ea7f6acba2da7d530796cf238c5
rolik00 <148611487+rolik00@users.noreply.github.com>
2024-05-16 11:21:53 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+# DO NOT EDIT
+import pytest
+
+from code/code import sum, power
+
+@pytest.mark.parametrize(
+    ('a', 'b', 'result'), [
+        (100, 87, 187),
+        (0, 0, 0),
+        (324, 2, 326),
+    ]
+)
+
+def test_sum(a, b, result):
+    assert sum(a, b) == result
+
+
+@pytest.mark.parametrize(
+    ('x', 'y', 'result'), [
+        (2, 3, 8),
+        (0, 0, 1),
+        (8, 3, 512),
+    ]
+)
+def test_power(x, y, result):
+    assert power(x, y) == result
